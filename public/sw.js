371f59353e18a84fa1529501b81f43a495e43671
Megiba Jasmine <megibajasmine@gmail.com>
2026-05-22 23:35:01 -0700

    Fix service worker and cache handling

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,37 @@
-self.addEventListener('install', e => {
-self.skipWaiting();
+const CACHE_NAME = "sjt-drive-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/drive/index.html",
+  "/drive/public.html",
+  "/drive/private.html"
+];
+
+self.addEventListener('install', event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+
+  );
+
 });
 
-self.addEventListener('fetch', event => {});
+self.addEventListener('fetch', event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+    .then(response => {
+
+      return response || fetch(event.request);
+
+    })
+
+  );
+
+});
